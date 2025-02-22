27d7e461bc852bcbde89859e81606cba63154ec8
EierMayo <e.g.edier.may@gmail.com>
2025-02-22 00:29:17 -0600

    StartingGridview

---
+++ lib/utils/donut_Tile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DonutTile extends StatelessWidget {
+  DonutTile({super.key});
+  final String donutFlavor;
+  final String donutPrice;
+  //dinamic  será un tipo de color
+  final dynamic donutColor;
+  final String imageName;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+    return co
+  }
+}
