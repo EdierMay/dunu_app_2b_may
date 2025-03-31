26ee75ca52588481a3424cf6e5073f9e5156bddc
EierMayo <e.g.edier.may@gmail.com>
2025-03-30 23:52:14 -0600

    AVANCE

--- lib/utils/donut_Tile.dart
+++ lib/utils/donut_Tile.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class DonutTile extends StatelessWidget {
+class DonutTile extends StatefulWidget {
   final String donutFlavor;
   final String donutStore;
   final String donutPrice;
-  //dynamic sera un tipo de color
   final dynamic donutColor;
   final String imageName;
-  const DonutTile(
-      {super.key,
-      required this.donutFlavor,
-      required this.donutStore,
-      required this.donutPrice,
-      this.donutColor,
-      required this.imageName});
+  final Function(String) onAddToCart;
+
+  const DonutTile({
+    super.key,
+    required this.donutFlavor,
+    required this.donutStore,
+    required this.donutPrice,
+    this.donutColor,
+    required this.imageName,
+    required this.onAddToCart,
+  });
+
+  @override
+  _DonutTileState createState() => _DonutTileState();
+}
+
+class _DonutTileState extends State<DonutTile> {
+  bool isFavorite = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,56 +32,53 @@ class DonutTile extends StatelessWidget {
       padding: const EdgeInsets.all(12.0),
       child: Container(
         decoration: BoxDecoration(
-            color: donutColor[50], borderRadius: BorderRadius.circular(24)),
+          color: widget.donutColor[50],
+          borderRadius: BorderRadius.circular(24),
+        ),
         child: Column(
           children: [
-            // PriceTag precio de la dona
             Row(
-              //alinea a la derecha
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(
                   decoration: BoxDecoration(
-                      color: donutColor[200],
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(24),
-                          bottomLeft: Radius.circular(24))),
+                    color: widget.donutColor[200],
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(24),
+                      bottomLeft: Radius.circular(24),
+                    ),
+                  ),
                   padding:
                       const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
-                  child: Text('\$$donutPrice',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: donutColor[800])),
-                )
+                  child: Text(
+                    '\$${widget.donutPrice}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: widget.donutColor[800],
+                    ),
+                  ),
+                ),
               ],
             ),
-
-            //Donut Picture imagen de la dona
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 34),
-              child: Image.asset(imageName),
+              child: Image.asset(widget.imageName),
             ),
-
-            //donut text, texto de la dona
-            Text(donutFlavor,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            SizedBox(
-              height: 4,
-            ),
-            // agregar texto de la tienda
             Text(
-              donutStore,
-              style: TextStyle(
+              widget.donutFlavor,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              widget.donutStore,
+              style: const TextStyle(
                 fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Color(0xFFBDBDBD), // Gris para el nombre de la tienda
+                fontSize: 16,
+                color: Color(0xFFBDBDBD),
               ),
             ),
-            SizedBox(
-              height: 4,
-            ),
-            //Love icon + add button, icono de me encanta e icono de agregar
+            const SizedBox(height: 4),
             Padding(
               padding: const EdgeInsets.all(12.0),
               child: Row(
@@ -79,18 +86,18 @@ class DonutTile extends StatelessWidget {
                 children: [
                   GestureDetector(
                     onTap: () {
-                      // Acción cuando se toca el ícono de corazón
-                      print("Corazón tocado");
+                      setState(() {
+                        isFavorite = !isFavorite;
+                      });
                     },
                     child: Icon(
-                      Icons.favorite_border,
+                      isFavorite ? Icons.favorite : Icons.favorite_border,
                       color: Colors.pink[400],
                     ),
                   ),
                   GestureDetector(
                     onTap: () {
-                      // Acción cuando se toca el texto "Add"
-                      print("Texto 'Add' tocado");
+                      widget.onAddToCart(widget.donutFlavor);
                     },
                     child: const Text(
                       "Add",
